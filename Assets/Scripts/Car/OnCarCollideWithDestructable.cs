9d550cdccaa67b61b1734d9780ca8df01a168086
Daniel <danielbc@hotmail.co.uk>
2020-07-11 21:03:28 +0100

    Treeees

--- Assets/Scripts/Car/OnCarCollideWithDestructable.cs
+++ Assets/Scripts/Car/OnCarCollideWithDestructable.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class OnCarCollideWithDestructable : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Destructable"))
+        if (other.gameObject.CompareTag("Collidable"))
         {
+            ObjectSwapOnCollide objectSwap = other.gameObject.GetComponent<ObjectSwapOnCollide>();
+            if (objectSwap != null && objectSwap.newObject != null)
+            {
+                GameObject newObj = Instantiate(objectSwap.newObject, other.transform.position, other.transform.rotation);
+                Rigidbody oldRB = other.GetComponent<Rigidbody>();
+                if (oldRB != null)
+                    foreach (Rigidbody rb in newObj.GetComponentsInChildren<Rigidbody>())
+                    {
+
+                        rb.AddExplosionForce(2000, transform.position + Vector3.left, 10);
+                    }
+            }
             Destroy(other.gameObject);
         }
     }
---
+++ Assets/Scripts/Collidable/ObjectSwapOnCollide.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSwapOnCollide : MonoBehaviour
+{
+    public GameObject newObject;
+}
