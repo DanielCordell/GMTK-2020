bd2e985c13273a72e50688190c83c8ba739a420f
Daniel <danielbc@hotmail.co.uk>
2020-07-12 01:48:45 +0100

    Destroyed animation works now

--- Assets/Scripts/General/DestroyAfterTime.cs
+++ Assets/Scripts/General/DestroyAfterTime.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DestroyAfterTime : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float startTime;
+    float shrinkTime;
+    public float timeTillShrinkStart;
+    public float shrinkSpeed;
+
+    Transform[] objectsToShrink;
+
+    float scale = 0.9f;
+
     void Start()
     {
+        startTime = Time.time;
+        shrinkTime = startTime + timeTillShrinkStart;
+        objectsToShrink = GetComponentsInChildren<Rigidbody>().Select(rb => rb.transform).ToArray();
         
+        // If there are no gameObjects to destroy, just sudoku.
+        if (objectsToShrink.Length == 0) Destroy(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time >= shrinkTime)
+        {
+            if (objectsToShrink.All(t => t.localScale.magnitude <= 0.01)) Destroy(gameObject);
+            else foreach (Transform t in objectsToShrink)
+            {
+                t.localScale *= scale;
+            }
+        }
     }
 }
